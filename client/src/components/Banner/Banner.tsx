42133493a61bc10be5a146c9962e12603635c5d1
soniikot <soniikot@gmail.com>
2024-09-11 17:46:02 -0700

    fix: img in banner got splashed

--- client/src/components/Banner/Banner.tsx
+++ client/src/components/Banner/Banner.tsx
@@ -7,17 +7,19 @@ export const Banner = () => {
     <div className="container">
       <div className={style.wrapper}>
         <div className={style.banner}>
-          <h2>WE MADE YOUR EVERYDAY FASHION BETTER</h2>
-          <p>
-            In our journey to improve everyday fashion, euphoria presents
-            EVERYDAY wear range - Comfortable & Affordable fashion 24/7
-          </p>
-          <div className={style.button}>
-            <TextButton text="Shop Now" />
+          <div className={style.text}>
+            <h2>WE MADE YOUR EVERYDAY FASHION BETTER</h2>
+            <p>
+              In our journey to improve everyday fashion, euphoria presents
+              EVERYDAY wear range - Comfortable & Affordable fashion 24/7
+            </p>
+            <div className={style.button}>
+              <TextButton text="Shop Now" />
+            </div>
           </div>
         </div>
         <div className={style.image_wrapper}>
-          {/* <img className={style.img} src={img} alt="group of people" /> */}
+          <img className={style.img} src={img} alt="group of people" />
         </div>
       </div>
     </div>
