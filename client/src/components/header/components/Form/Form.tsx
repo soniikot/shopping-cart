86af64ebc8074209985df3c84c8fc8aa36f5d33a
soniikot <soniikot@gmail.com>
2024-09-24 18:30:36 -0700

    fix form

--- client/src/components/header/components/Form/Form.tsx
+++ client/src/components/header/components/Form/Form.tsx
@@ -1,19 +1,16 @@
 import style from './styles.module.scss';
 import heart from '@/assets/heart.svg';
 import user from '@/assets/user.svg';
-import shoppingCart from '@/assets/shopping-cart.svg';
+import shoppingCart from '@/assets/user.svg';
 import { IconButton } from '@/shared/components/IconButton/IconButton';
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
 
 export const Form: FC = () => {
   return (
     <div className={style.form}>
       <IconButton icon={heart} />
       <IconButton icon={user} />
-      <Link to="/cart">
-        <IconButton icon={shoppingCart} />
-      </Link>
+      <IconButton icon={shoppingCart} />
     </div>
   );
 };
