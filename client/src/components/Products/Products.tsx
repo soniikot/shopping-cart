ce777a55b69c3ee0814d3027741fb21f3ac3fd22
soniikot <soniikot@gmail.com>
2024-10-12 21:55:57 -0700

    fix styles

--- client/src/components/Products/Products.tsx
+++ client/src/components/Products/Products.tsx
@@ -3,6 +3,8 @@ import { FC } from 'react';
 import { useAppSelector } from '@/app/hooks';
 import { selectProducts } from '@/features/products/productsSlice';
 import { EmptyList } from '../EmptyList/EmptyList';
+import { Link } from 'react-router-dom';
+
 export interface ProductsTypeProps {
   numberOfProducts: number;
 }
@@ -16,22 +18,24 @@ export const Products: FC<ProductsTypeProps> = ({ numberOfProducts }) => {
     <div className="container">
       <div className={style.wrapper}>
         {filteredProducts.slice(0, numberOfProducts).map((product) => (
-          <div key={product.id} className={style.card}>
-            <img
-              className={style.img}
-              src={
-                import.meta.env.VITE_API_UPLOAD_URL +
-                product.attributes.img.data.attributes.url
-              }
-              alt={product.attributes.title}
-            />
+          <Link to={`/product/${product.id - 1}`} key={product.id}>
+            <div key={product.id} className={style.card}>
+              <img
+                className={style.img}
+                src={
+                  import.meta.env.VITE_API_UPLOAD_URL +
+                  product.attributes.img.data.attributes.url
+                }
+                alt={product.attributes.title}
+              />
 
-            <div className={style.text_wrapper}>
-              <p className={style.title}>{product.attributes.title}</p>
-              <p className={style.subtitle}>{product.attributes.disc}</p>
+              <div className={style.text_wrapper}>
+                <p className={style.title}>{product.attributes.title}</p>
+                <p className={style.subtitle}>{product.attributes.disc}</p>
+              </div>
+              <div className={style.price}>${product.attributes.price}</div>
             </div>
-            <div className={style.price}>${product.attributes.price}</div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
